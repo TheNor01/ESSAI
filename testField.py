3e5c46c624cdb480f4171dc930f8f9016b983798
Aldo Fiorito <aldo.fiorito97@gmail.com>
2024-01-10 16:42:40 +0100

    cleaning

--- testField.py
+++ testField.py
@@ -5,7 +5,7 @@ from sentence_transformers import SentenceTransformer
 from keywords_suggester.bin.modules.ChromaSingle import ChromaClass
 from keywords_suggester.bin.modules.RetriverSingle import RetrieverSingle
 import pickle
-
+from keywords_suggester.bin.modules.LLModel import LLModel
 
 settings.init()
 persist_directory = settings.persist_directory+"init_dataset"+"/"
@@ -36,3 +36,17 @@ ChromaDB = ChromaClass(persist_directory,embed_model,collection_name_local)
 
 ChromaDB.HistogramUsersTopics()
 #BERT.Genereate_WC(11)
+
+mygpt = LLModel(ChromaDB)
+
+#docs = mygpt.SelfQuery("I want to know what article user 4a2bd reads")
+#docs = mygpt.SelfQuery("Give me some food documents created in year 2024")
+#docs = mygpt.SelfQuery("Based on his documents, create a sample text for the user dc16c")
+#docs = mygpt.SelfQuery("What are some documents about food which contains the word chicken")
+#docs = mygpt.StructuredQuery("give me sports documents with creation date equals to 2023-06-02. You have to treat date as string")
+
+#docs = mygpt.retriever.invoke("I want to know what article user 4a2bd reads")
+#pretty_print_docs(docs)
+
+
+docs = mygpt.RagQA("Give me 5 healthy food")
--- upload_batch_docs.py
+++ upload_batch_docs.py
@@ -28,12 +28,8 @@ from langchain.document_loaders import UnstructuredURLLoader
 if __name__ == '__main__':
 
 
-    #TODO AGGIUMGERE PICCOLA SEZIONE STREAMING
-    #https://faust.readthedocs.io/en/latest/
-
 
     #TODO capire se conviene fare prima la preview e poi classificare o al contrario
-
 
     settings.init()
     persist_directory = settings.persist_directory+settings.init_dataset+"/"
@@ -79,43 +75,6 @@ if __name__ == '__main__':
     upload_df = build_dataframe_from_csv_uploaded(BERT,CHOOSED_FILE)
     print(upload_df)
 
-    """
-    topic_info = BERT.main_model.get_topic_info()
-    
-    dict_topic_name = dict(zip(topic_info['Topic'], topic_info['Name']))
-    
-    documents_list,topics_list,users_list,ids_list = [],[],[],[]
-    CREATED_TIME_NOW =  datetime.now().strftime("%Y-%m-%d")
-    
-    
-    
-    with open(os.path.join(settings.upload_directory,SELECTED_UPLOAD)) as file_obj: 
-        reader_obj = csv.reader(file_obj,delimiter="|") 
-        next(reader_obj, None) # SKIP HEADERS
-        for row in reader_obj: 
-            local_doc = row[1]
-            ids_list.append(hashlib.md5(local_doc.encode()))
-            documents_list.append(local_doc)
-            users_list.append(row[0])
-            print(local_doc)
-            
-            topics, probs = BERT.main_model.transform(local_doc)
-            print(topics) #topics di zero dovrebbe essere il massimo
-            print(probs)
-            #index = np.argmax(probs)
-            max_topic = topics[0]
-            
-            #print(probs.shape)
-            #print(probs)
-            #topic_mapped = [dict_topic_name[key] for key in topics]
-            topic_mapped = dict_topic_name[max_topic] 
-            topics_list.append(topic_mapped)
-
-    upload_df = pd.DataFrame(zip(documents_list, topics_list, users_list,ids_list),columns=['content','category', 'user','ids'])
-    upload_df['created_at']=CREATED_TIME_NOW
-    #create column metadata as dict
-    """
-    
 
     metadata_df = upload_df[["user","category","created_at"]]
     metadata_dict = metadata_df.to_dict(orient='records')
